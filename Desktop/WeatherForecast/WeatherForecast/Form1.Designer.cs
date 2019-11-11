c59374beccc3d6cecc35fa760db8c99ec2e9d218
FURKAN YANMAZ <furkan14991499@gmail.com>
2019-11-11 15:59:54 +0300

    Hava durumu

---
+++ Desktop/WeatherForecast/WeatherForecast/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WeatherForecast
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewList = new System.Windows.Forms.DataGridView();
+            this.cmbCityName = new System.Windows.Forms.ComboBox();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewList
+            // 
+            this.dataGridViewList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewList.Location = new System.Drawing.Point(92, 140);
+            this.dataGridViewList.Name = "dataGridViewList";
+            this.dataGridViewList.Size = new System.Drawing.Size(465, 260);
+            this.dataGridViewList.TabIndex = 0;
+            // 
+            // cmbCityName
+            // 
+            this.cmbCityName.FormattingEnabled = true;
+            this.cmbCityName.Location = new System.Drawing.Point(92, 56);
+            this.cmbCityName.Name = "cmbCityName";
+            this.cmbCityName.Size = new System.Drawing.Size(200, 21);
+            this.cmbCityName.TabIndex = 1;
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(92, 92);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(200, 20);
+            this.date.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(347, 54);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Ara";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(522, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(522, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.cmbCityName);
+            this.Controls.Add(this.dataGridViewList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewList;
+        private System.Windows.Forms.ComboBox cmbCityName;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Desktop/WeatherForecast/WeatherForecast/Form1.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherForecast
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AddCity();
+
+        }
+        public void AddCity()
+        {
+            cmbCityName.Items.Add("İstanbul");
+            cmbCityName.Items.Add("Paris");
+            cmbCityName.Items.Add("London");
+            cmbCityName.Items.Add("Ankara");
+            cmbCityName.Items.Add("İzmir");
+        }
+
+        public class City
+        {
+            public int woeid { get; set; }
+            public string title { get; set; }
+        }
+        public class Weather
+        {
+            public string id { get; set; }
+            public double the_temp { get; set; }
+
+        }
+
+        private async void BtnSearch_Click(object sender, EventArgs e)
+        {
+            //    var client = new HttpClient();
+            //    client.BaseAddress = new Uri("https://www.metaweather.com/");
+            //    HttpResponseMessage response = await client.GetAsync("/api/location/");
+            //    string result = await response.Content.ReadAsStringAsync();
+            //    label1.Text = result;
+            try
+            {
+                WebClient client = new WebClient();
+                client.Encoding = Encoding.UTF8;
+
+                string url = "https://www.metaweather.com/api/location/search/?query=";
+                string city = cmbCityName.Text;
+                string Text = client.DownloadString(url + city);
+                JArray lst = JArray.Parse(Text);
+                City nesne = JsonConvert.DeserializeObject<City>(lst[0].ToString());
+                //label1.Text = nesne.title + "  " + nesne.woeid;
+
+                WebClient client2 = new WebClient();
+                client2.Encoding = Encoding.UTF8;
+                string url2 = "https://www.metaweather.com/api/location/";
+                string id = nesne.woeid.ToString();
+                int year = date.Value.Year;
+                int mount = date.Value.Month;
+                int day = date.Value.Day;
+                string url3 = url2 + id + "/" + year.ToString() + "/" + mount.ToString() + "/" + day.ToString();
+                string Text2 = client2.DownloadString(url3);
+                JArray lst2 = JArray.Parse(Text2);
+                Weather nesne2 = JsonConvert.DeserializeObject<Weather>(lst2[0].ToString());
+                //label2.Text = nesne2.id + " " + nesne2.the_temp;
+
+                dataGridViewList.ReadOnly = true;
+                
+                dataGridViewList.DataSource = lst2;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+               
+            }
+        }
+    }
+}
